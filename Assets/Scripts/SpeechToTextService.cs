0dc3c42f6eb738ea7e268c6d29fed5f2009bd3e4
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-02 10:23:17 +0200

    add custom profiles

---
+++ Assets/Scripts/SpeechToTextService.cs
@@ -0,0 +1,13 @@
+using XRTK.Definitions;
+using XRTK.Interfaces;
+using XRTK.Services;
+
+public class SpeechToTextService : BaseExtensionService  {
+
+    public SpeechToTextService(string name, uint priority, BaseMixedRealityExtensionServiceProfile profile) : base(name, priority, profile) { }
+    
+    public override void Enable() {
+            
+            
+    }
+}
