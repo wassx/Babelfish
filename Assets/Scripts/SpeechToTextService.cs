42ccfae99d52ad5206456ca101693c981e703ab2
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-02 11:07:43 +0200

    Add first speech recognition

--- Assets/Scripts/SpeechToTextService.cs
+++ Assets/Scripts/SpeechToTextService.cs
@@ -1,13 +1,42 @@
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
 using XRTK.Definitions;
-using XRTK.Interfaces;
 using XRTK.Services;
 
 public class SpeechToTextService : BaseExtensionService  {
+    private readonly SpeechConfig _config = SpeechConfig.FromSubscription("433296e9a13c48928cdeef3d4d1433d1", "northeurope");
+    private readonly object _threadLocker = new object();
+    private bool waitingForReco;
+    private string _message;
+
+    private bool _micPermissionGranted = false;
 
     public SpeechToTextService(string name, uint priority, BaseMixedRealityExtensionServiceProfile profile) : base(name, priority, profile) { }
     
-    public override void Enable() {
-            
-            
+    public async Task<string> RecognizeSpeech() {
+        using (var recognizer = new SpeechRecognizer(_config)) {
+            lock (_threadLocker) {
+                waitingForReco = true;
+            }
+
+            var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+            string newMessage = string.Empty;
+            if (result.Reason == ResultReason.RecognizedSpeech) {
+                newMessage = result.Text;
+            } else if (result.Reason == ResultReason.NoMatch) {
+                newMessage = "NOMATCH: Speech could not be recognized.";
+            } else if (result.Reason == ResultReason.Canceled) {
+                var cancellation = CancellationDetails.FromResult(result);
+                newMessage = $"CANCELED: Reason={cancellation.Reason} ErrorDetails={cancellation.ErrorDetails}";
+            }
+
+            lock (_threadLocker) {
+                _message = newMessage;
+                waitingForReco = false;
+            }
+
+            return _message;
+        }
     }
 }
