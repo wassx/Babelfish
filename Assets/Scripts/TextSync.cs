0990177e54948f17109f3053b0e53928297cfd92
Philipp Raab <philipp.raab@dynatrace.com>
2019-09-02 10:57:03 +0200

    Add text sync

---
+++ Assets/Scripts/TextSync.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class TextSync : MonoBehaviour, IPunObservable {
+    [SerializeField] private TextMeshPro _text;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.IsWriting) {
+            stream.SendNext(_text.text);
+        } else {
+            _text.text = (string) stream.ReceiveNext();
+        }
+    }
+
+    public void SetText(string text) {
+        _text.text = text;
+    }
+}
