e3404aee7c74d8670a6f6b090a2309b2360c91a8
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-02 12:40:19 +0200

    Click cube to start speechrecognition

---
+++ Assets/Scripts/CubeClickhandler.cs
@@ -0,0 +1,11 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using XRTK.Utilities;
+
+public class CubeClickhandler : MonoBehaviour {
+    public void OnCubeClicked() {
+        Babbelfish babbelfish = CameraCache.Main.GetComponentInChildren<Babbelfish>();
+        babbelfish.OnStartSpeech();
+    }
+}
