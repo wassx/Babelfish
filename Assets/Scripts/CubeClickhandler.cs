1eaef1fe112da2926407be9782469afb3b93085f
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-02 12:54:00 +0200

    Added cube click feedback

--- Assets/Scripts/CubeClickhandler.cs
+++ Assets/Scripts/CubeClickhandler.cs
@@ -1,11 +1,23 @@
-﻿using Photon.Pun;
-using Photon.Realtime;
-using UnityEngine;
+﻿using UnityEngine;
 using XRTK.Utilities;
+using XRTK.Utilities.Async;
 
 public class CubeClickhandler : MonoBehaviour {
-    public void OnCubeClicked() {
+    private Renderer _renderer;
+    private Color _active = new Color(0.8f,0.8f,0.9f);
+    private Color _listening = new Color(0.3f,0.4f,1f);
+
+    private void Start() {
+        _renderer = gameObject.GetComponent<Renderer>();
+        _renderer.material.color = _active;
+    }
+
+    public async void OnCubeClicked() {
+        _renderer.material.color = _listening;
         Babbelfish babbelfish = CameraCache.Main.GetComponentInChildren<Babbelfish>();
         babbelfish.OnStartSpeech();
+        await new WaitForSeconds(2);
+        _renderer.material.color = _active;
+        
     }
 }
