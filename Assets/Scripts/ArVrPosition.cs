bfc684dd3818f3063db72c984e2ab17529052a1a
stefan.wasserbauer <stefan.wasserbauer@dynatrace.com>
2019-09-03 13:00:04 +0200

    Cleanup, AR head position

---
+++ Assets/Scripts/ArVrPosition.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA;
+
+public class ArVrPosition : MonoBehaviour {
+    [SerializeField] private Vector3 _arPosition = new Vector3(0, 1.7f, 0);
+    [SerializeField] private Vector3 _vrPosition = new Vector3(0, 0, 0);
+
+    private void Awake() {
+        transform.localPosition = HolographicSettings.IsDisplayOpaque
+            ? _vrPosition
+            : _arPosition;
+    }
+}
